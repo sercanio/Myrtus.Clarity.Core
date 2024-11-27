dd876b4b05b5f3d69baf1ff70f690239e16f241c
sercanio <104576153+sercanio@users.noreply.github.com>
2024-11-27 15:39:37 +0300

    feat! Add new mailing service with MailKit implementation
    Introduced a new mailing service to the project.
    
    1. Added `IMailService` interface in `Myrtus.Clarity.Core.Application.Abstractions.Mailing` namespace.
    2. Created `Myrtus.Clarity.Core.Application.Abstractions.Mailing.csproj` targeting .NET 9.0.
    3. Added `Mail` class in `Myrtus.Clarity.Core.Domain.Abstractions.Mailing` namespace.
    4. Added `MailSettings` class in `Myrtus.Clarity.Core.Domain.Abstractions.Mailing` namespace.
    5. Added `ToEmail` class in `Myrtus.Clarity.Core.Domain.Abstractions.Mailing` namespace.
    6. Updated `Myrtus.Clarity.Core.Domain.Abstractions.csproj` to include `MimeKit` package.
    7. Implemented `MailKitMailService` class in `Myrtus.Clarity.Core.Infrastructure.Mailing.MailKit` namespace.
    8. Created `Myrtus.Clarity.Core.Infrastructure.Mailing.MailKit.csproj` targeting .NET 9.0 with necessary package and project references.

---
+++ Myrtus.Clarity.Core.Application.Abstractions.Mailing/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using Myrtus.Clarity.Core.Domain.Abstractions.Mailing;
+
+namespace Myrtus.Clarity.Core.Application.Abstractions.Mailing
+{
+    public interface IMailService
+    {
+        void SendMail(Mail mail);
+        Task SendEmailAsync(Mail mail);
+    }
+}
