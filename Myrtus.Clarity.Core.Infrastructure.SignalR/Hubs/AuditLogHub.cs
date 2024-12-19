9560dca5c1f256a59225cbbb6efc1e2d65fc572b
sercanio <104576153+sercanio@users.noreply.github.com>
2024-12-19 18:41:56 +0300

    feat!: Add authorization to SendAuditLog method in AuditLogHub
    Introduce authorization requirement for SendAuditLog method to ensure only authorized users can send audit log messages. Added necessary using directive for authorization and reordered existing using directives.

--- Myrtus.Clarity.Core.Infrastructure.SignalR/Hubs/AuditLogHub.cs
+++ Myrtus.Clarity.Core.Infrastructure.SignalR/Hubs/AuditLogHub.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Myrtus.Clarity.Core.Infrastructure.SignalR.Hubs;
 
 public class AuditLogHub : Hub
 {
+    [Authorize]
     public async Task SendAuditLog(string message)
     {
         await Clients.All.SendAsync("ReceiveAuditLog", message);
