2565243d1154e7f81263622b50aac884e567197d
sercanio <104576153+sercanio@users.noreply.github.com>
2024-12-30 17:54:24 +0300

    feat: Add IClarityModule interface and new project file
    Introduce IClarityModule interface in Myrtus.Clarity.Core.Application.Abstractions.Module namespace with ConfigureServices and Configure methods for service and pipeline configuration. Add Myrtus.Clarity.Core.Application.Abstractions.Module.csproj targeting .NET 9.0 with implicit usings, nullable reference types, and package references to Microsoft.AspNetCore.Http.Abstractions 2.2.0, Microsoft.Extensions.Configuration.Abstractions 9.0.0, and Microsoft.Extensions.DependencyInjection.Abstractions 9.0.0.

---
+++ Myrtus.Clarity.Core.Application.Abstractions.Module/IClarityModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Myrtus.Clarity.Core.Application.Abstractions.Module;
+
+public interface IClarityModule
+{
+    void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+    void Configure(IApplicationBuilder app);
+}
