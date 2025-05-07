9aea819bc732ec2c7382162653a28f151d24fea7
sercanio <104576153+sercanio@users.noreply.github.com>
2025-05-07 21:34:19 +0300

    refactor: move notification and auditlog features from MongoDb to PostgreSQL and remove module support

--- Myrtus.Clarity.Core.Application.Abstractions.Auditing/IAuditLogService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Myrtus.Clarity.Core.Domain.Abstractions;
-
-namespace Myrtus.Clarity.Core.Application.Abstractions.Auditing;
-
-public interface IAuditLogService
-{
-    Task LogAsync(AuditLog log);
-}
